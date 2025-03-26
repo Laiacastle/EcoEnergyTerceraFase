6a0a6a6029890f0401f4c5e25961726b4969b4b2
Laiacastle <laia.castillo.7e6@itb.cat>
2025-03-26 20:11:58 +0100

    Afegir l'opcio d'esborrar a totes les paginas. Closes #5

--- EcoEnergySegonaFaseDef/Pages/Delete.cshtml.cs
+++ EcoEnergySegonaFaseDef/Pages/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using EcoEnergyTerceraFase.Data;
+using EcoEnergyTerceraFase.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,40 @@ namespace EcoEnergyTerceraFase.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet(int? simulation, int? consume, int? indicator)
         {
+            using var context = new ApplicationDbContext();
+            if (simulation != null)
+            {
+                if (context.Simulacions.Find(simulation) != null)
+                {
+                    Simulacions sim = context.Simulacions.Find(simulation);
+                    context.Simulacions.Remove(sim);
+                    context.SaveChanges();
+                }
+                return RedirectToPage("./Simulations/Simulations");
+            }
+            else if (consume != null)
+            {
+                if(context.Consums.Find(consume) != null)
+                {
+                    ConsumAigua cons = context.Consums.Find(consume);
+                    context.Consums.Remove(cons);
+                    context.SaveChanges();
+                }
+                return RedirectToPage("./WaterConsumption/WaterConsumes");
+            }
+            else if(indicator != null)
+            {
+                if(context.Indicadors.Find(indicator) != null)
+                {
+                    IndicadorsEnergetics indi = context.Indicadors.Find(indicator);
+                    context.Indicadors.Remove(indi);
+                    context.SaveChanges();
+                }
+                return RedirectToPage("./EnergyIndicators/EnergyIndicators");
+            }
+            return RedirectToPage("Index");
         }
     }
 }
